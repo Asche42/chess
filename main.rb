7d158ea7c05fac4cb6b1a3e4dfa9c3999b2092dd
Thomas Charbonnel <thomas@charbonnel.eu>
2016-11-14 00:33:56 +0800

    add options to menu

--- main.rb
+++ main.rb
@@ -10,8 +10,8 @@ valid_square = /^[a-hA-H][1-8]$/
 def print_help
   puts "    To get info on a piece, enter the square. E.g.\n    > h8"
   puts "    To move a piece, type origin then destination with a space in between. E.g.\n    > b2 b3"
-  puts "    To display the board, type “display”, “show”, “print” or “p”."
-  puts "    To exit, type “quit” or “exit”."
+  puts "    To display the board, type “display”, “show”, “print”, “p” or “s”."
+  puts "    To exit, type “quit”, “exit” or “q”."
 end
 
 line = 1
@@ -19,11 +19,11 @@ loop do
   print "> "
   input = gets.chomp
 
-  break if %w(quit exit).include?(input)
+  break if %w(quit exit q).include?(input)
 
   if valid_square =~ input
     puts board.get_piece_at(input) || "There is no piece on #{input}."
-  elsif %w(display show print p).include?(input)
+  elsif %w(display show print p s).include?(input)
     puts board
   elsif %w(help ?).include?(input)
     print_help
